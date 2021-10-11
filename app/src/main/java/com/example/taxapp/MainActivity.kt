523222904a7d89f017395767a87c9a0575c4abba
lambersonistaken <ismail.lambacioglu@org.sakarya.edu.tr>
2021-10-11 21:24:54 +0300

    Simple Tax App

---
+++ app/src/main/java/com/example/taxapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.taxapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taxapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.hesaplaButton.setOnClickListener{
+
+            kdvHesapla()
+        }
+    }
+
+fun kdvHesapla(){
+
+    val urunFiyatiString = binding.Fiyat.text.toString()
+
+    val tutar = urunFiyatiString.toDouble()
+
+    val kdvHesaplanan = tutar * 0.18
+
+    val kdvHesaplananString = kdvHesaplanan.toString()
+
+    val kdvDahil = kdvHesaplanan + tutar
+
+    val kdvDahilString = kdvDahil.toString()
+
+
+    binding.kdvOncesi.text = "$urunFiyatiString ₺"
+    binding.kdvMiktari.text = "$kdvHesaplananString ₺"
+    binding.kdvDahilFiyat.text = "$kdvDahilString ₺"
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
